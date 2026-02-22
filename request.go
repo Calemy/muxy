5b0ce986317a323b195dfdd264c06fbf0e83f557
Nanoo <45440100+Calemy@users.noreply.github.com>
2026-02-22 08:24:52 +0100

    add query by array method

--- request.go
+++ request.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net"
 	"net/http"
+	"strings"
 )
 
 type Request struct {
@@ -21,11 +22,54 @@ func (r *Request) check(sFunc func(string) string, keys ...string) string {
 		}
 	}
 
-	if len(keys) < 2 { // * If there is no default value, return empty string
+	if len(keys) < 2 { //* If there is no default value, return empty string
 		return ""
 	}
 
-	return keys[len(keys)-1] // * Returns last key as default value
+	return keys[len(keys)-1] //* Returns last key as default value
+}
+
+func (r *Request) checkArray(keys ...string) []string {
+	if len(keys) == 0 {
+		panic("muxy: need to provide at least one key to check")
+	}
+
+	queries := make(map[string][]string)
+	query := r.URL.RawQuery
+
+	for _, pair := range strings.Split(query, "&") {
+		kv := strings.SplitN(pair, "=", 2) //* Splits the query pair into key and value
+		if len(kv) != 2 {
+			continue
+		}
+
+		if kv[1] == "" { //* If the value is empty, we ignore it and move on
+			continue
+		}
+
+		if _, ok := queries[kv[0]]; !ok {
+			queries[kv[0]] = make([]string, 0)
+		}
+
+		queries[kv[0]] = append(queries[kv[0]], kv[1])
+	}
+
+	for _, key := range keys {
+		if values, ok := queries[key]; ok {
+			return values
+		}
+	}
+	return make([]string, 0)
+}
+
+func (r *Request) Queries(keys ...string) []string {
+	value := r.checkArray(keys...)
+	if len(value) == 0 { //* If the key is not found, return empty array
+		return value
+	} else if len(value) == 1 { //* If the key is found and has only one value, we check if it was tried to be seperated by comma.
+		return strings.Split(value[0], ",")
+	}
+	return value
 }
 
 func (r *Request) Query(keys ...string) string {
